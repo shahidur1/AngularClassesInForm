60eeceeb07f4a0dbaed009627a84a33e6ea9af75
shahidur1 <idbbisew01@gmail.com>
2020-04-03 17:15:55 +0600

    first2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularclasses';
+  skill: string[] = ["R10", "R11", "R12"];
+  address: Address = new Address("Bashbari", "mohammadpur", "Dhaka", 1215);
+  user: User = new User(this.address, "100", "sss", "ss@gmail.com", "R12", "female", false, true);
+  public m1(data): void {
+    console.log(data);
+  }
+}
+export class User {
+  address: Address;
+  id: string;
+  name: string;
+  email: string;
+  round: string;
+  gender: string;
+  skilljava: boolean;
+  skillcs: boolean;
+  constructor(addrress: Address, id: string, name: string, email: string, round: string, gender: string, skilljava: boolean, skillcs: boolean) {
+    this.address = addrress;
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.round = round;
+    this.gender = gender;
+    this.skilljava = skilljava;
+    this.skillcs = skillcs;
+  }
+}
+export class Address {
+  street: string;
+  city: string;
+  state: string;
+  postalcode: number;
+  constructor(street: string, city: string, state: string, postalcode: number) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.postalcode = postalcode;
+  }
 }
